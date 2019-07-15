aaf521ef0c80bb6960314abeb8768d7533481d6a
vincentouma <vinceoumah@gmail.com>
2019-07-15 03:45:16 +0300

    Add indentations and syntax corrections

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {User} from '../user';
-import {ProfileService} from '../profiles/profile.service';
-import {Repo} from '../repo';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { ProfileService } from '../profiles/profile.service';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
-  providers:[ProfileService]
+  providers: [ProfileService]
 })
 
 export class ProfileComponent implements OnInit {
-    
-  user: User;
-  
- constructor(private profileService: ProfileService, public repoService: ProfileService) { 
- this.user = this.profileService.user;
- }
- ngOnInit() {
-  // this.profileService.getProfileInfo(this.username);
-  this.user = this.profileService.user;
 
-}
+  user: User;
+
+  constructor(private profileService: ProfileService, public repoService: ProfileService) {
+    this.user = this.profileService.user;
+  }
+  ngOnInit() {
+    // this.profileService.getProfileInfo(this.username);
+    this.user = this.profileService.user;
+
+  }
 }
